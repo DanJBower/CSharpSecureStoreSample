4c957689e2a4ef5748b08d59ae3c98c87e26d7a0
DanJBower <bowerdanielj@gmail.com>
2023-02-12 22:20:59 +0000

    Added encryption decryption sample

---
+++ VeryBasicPatDemo/VeryBasicPatDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Security;
+using VeryBasicPatDemo;
+
+// User would normally enter this data directly into the secure string to reduce time it is in memory.
+// However, to keep this sample simple, this string will represent the user inputting data.
+var dataToEncrypt = "MyTestPassword";
+
+Console.WriteLine($"Encrypting \"{dataToEncrypt}\" and storing it");
+using var secureString = new SecureString();
+foreach (var c in dataToEncrypt)
+{
+    secureString.AppendChar(c);
+}
+Sample.StorePat(secureString);
+
+Console.WriteLine("Decrypting PAT from storage");
+using var retrievedValue = Sample.RetrievePat();
+Console.WriteLine($"Decrypted PAT: {new NetworkCredential("", retrievedValue).Password}");
---
+++ VeryBasicPatDemo/VeryBasicPatDemo/Sample.cs
@@ -0,0 +1,55 @@
+﻿using System.IO.IsolatedStorage;
+using System.Net;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VeryBasicPatDemo;
+
+public static class Sample
+{
+    private static readonly byte[] Entropy = "MyApplicationEntropy"u8.ToArray();
+
+    public static SecureString RetrievePat()
+    {
+        // Get the PAT from storage
+        var storageArea = IsolatedStorageFile.GetUserStoreForApplication();
+        if (!storageArea.FileExists("MyApplicationPatStorage.secret"))
+        {
+            throw new FileNotFoundException("Not encrypted PAT data found");
+        }
+        using var patStorage = new IsolatedStorageFileStream("MyApplicationPatStorage.secret", FileMode.Open, storageArea);
+        using var encryptedDataStream = new MemoryStream();
+        patStorage.CopyTo(encryptedDataStream);
+        var encryptedData = encryptedDataStream.ToArray();
+
+        // Decrypt it
+        var patData = ProtectedData.Unprotect(encryptedData, Entropy, DataProtectionScope.CurrentUser);
+        var plainTextPat = Encoding.UTF8.GetString(patData);
+
+        // Return it to user
+        SecureString result = new();
+        foreach (var c in plainTextPat)
+        {
+            result.AppendChar(c);
+        }
+        GC.Collect(); // Force remove unprotected string data from memory
+        return result;
+    }
+
+    public static void StorePat(SecureString pat)
+    {
+        // Get the PAT to encrypt
+        var plainTextPat = new NetworkCredential("", pat).Password;
+        var patData = Encoding.UTF8.GetBytes(plainTextPat);
+
+        // Encrypt it
+        var encryptedData = ProtectedData.Protect(patData, Entropy, DataProtectionScope.CurrentUser);
+        GC.Collect(); // Force remove unprotected string data from memory
+
+        // Store it
+        var storageArea = IsolatedStorageFile.GetUserStoreForApplication();
+        using var patStorage = new IsolatedStorageFileStream("MyApplicationPatStorage.secret", FileMode.Create, storageArea);
+        patStorage.Write(encryptedData);
+    }
+}
